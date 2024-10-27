ef86ed7b8b170674c0c7e206beb48cb98024ecba
Jairopln <87547016+jairomatheus89@users.noreply.github.com>
2024-10-27 00:01:52 -0300

    atualizaçãoToDoList

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const sendButton = document.getElementById("sendBut");
 const listDiv = document.getElementById("listDiv");
+const warningDiv = document.getElementById("WarningDiv");
+
+const tasksArray = [];
+
 
 sendButton.addEventListener('click', function() {
+
+    let inputTask = document.getElementById("inputTask").value;
+
     const tasksConteiners = document.createElement("tasksConteiners");
     const tasksNames = document.createElement("tasksNames");
     const deleteTaskBut = document.createElement("delTaskBut");
-    
+
     tasksConteiners.classList.add("tasksConteiners");
     tasksNames.classList.add("tasksNames");
     deleteTaskBut.classList.add("delTaskBut");
 
-    tasksConteiners.appendChild(tasksNames);
-    tasksConteiners.appendChild(deleteTaskBut);
-    listDiv.append(tasksConteiners);
+    if(inputTask.trim() === "") {
+        console.log("ta vazio otario");
+
+        warningDiv.classList.remove("warningClass");
+
+        void warningDiv.offsetWidth;
+
+        warningDiv.innerText = "Insert your task first!";
+
+        warningDiv.classList.add("warningClass");
+
+    } else {
+        tasksNames.innerText = inputTask;
+
+        
+        console.log("tem conteudo");
+        warningDiv.innerText = "";
+        listDiv.append(tasksConteiners);
+        tasksConteiners.appendChild(tasksNames);
+        tasksConteiners.appendChild(deleteTaskBut);
+    }    
+
 });
